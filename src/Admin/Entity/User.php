6b133a9f3ffe7301297361e44da2464cdb8245ec
Björn Bartels <bartels@dragon-projects.net>
2016-02-19 13:55:50 +0100

    update (zfc)user entity object

--- src/Admin/Entity/User.php
+++ src/Admin/Entity/User.php
@@ -196,33 +196,40 @@ class User extends ZfcUser
 	}
 	
 	
-	
-	public function exchangeArray($data)
+	/**
+	 * set user's object property data
+	 * @param array $data
+	 */
+	public function exchangeArray($data = array())
 	{
-		$this->id		    = (isset($data['id'])) ? $data['id'] : null;
-		$this->user_id		= (isset($data['user_id'])) ? $data['user_id'] : null;
-		$this->username		= (isset($data['username'])) ? $data['username'] : null;
-		$this->email		= (isset($data['email'])) ? $data['email'] : null;
-		$this->street		= (isset($data['street'])) ? $data['street'] : '';
-		$this->city			= (isset($data['city'])) ? $data['city'] : '';
-		$this->phone		= (isset($data['phone'])) ? $data['phone'] : '';
+		if ( isset($data['id']) && !empty($data["id"]) ) {
+			$this->id		    = isset($data['id']);
+			$this->user_id		= isset($data['id']);
+		} elseif ( isset($data['user_id']) && !empty($data["user_id"]) ) {
+			$this->id		    = isset($data['user_id']);
+			$this->user_id		= isset($data['user_id']);
+		}
+		$this->username		= (isset($data['username'])) ? $data['username'] : $this->username;
+		$this->email		= (isset($data['email'])) ? $data['email'] : $this->email;
+		$this->street		= (isset($data['street'])) ? $data['street'] : $this->street;
+		$this->city			= (isset($data['city'])) ? $data['city'] : $this->city;
+		$this->phone		= (isset($data['phone'])) ? $data['phone'] : $this->phone;
 		/* $bcrypt = new Bcrypt;
 		$bcrypt->setCost(null); // @TODO $this->getUserService()->getOptions()->getPasswordCost());
 		$this->password		= (isset($data['password'])) ? $bcrypt->create($data['password']) : null; */
-		$this->password		= (isset($data['password'])) ? $data['password'] : null;
-		$this->display_name	= (isset($data['display_name'])) ? $data['display_name'] : '';
-		$this->state		= (isset($data['state'])) ? $data['state'] : 0;
-		$this->aclrole		= (isset($data['aclrole'])) ? $data['aclrole'] : 'public';
-		$this->token		= (isset($data['token'])) ? $data['token'] : '';
+		$this->password		= (isset($data['password'])) ? $data['password'] : $this->password;
+		$this->display_name	= (isset($data['display_name'])) ? $data['display_name'] : $this->display_name;
+		$this->displayName	= (isset($data['display_name'])) ? $data['display_name'] : $this->displayName;
+		$this->state		= (isset($data['state'])) ? $data['state'] : $this->state;
+		$this->aclrole		= (isset($data['aclrole'])) ? $data['aclrole'] : $this->aclrole;
+		$this->token		= (isset($data['token'])) ? $data['token'] : $this->token;
+		return $this;
 	}
-
-	/*public function getArrayCopy()
-	{
-		return get_object_vars($this);
-	}*/
 	
-	
-	
+	/**
+	 * get copy of user's object properties in an assosiative array
+	 * @return array
+	 */
 	public function __getArrayCopy()
 	{
 		return array(
