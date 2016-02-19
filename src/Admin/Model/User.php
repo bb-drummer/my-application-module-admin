3f71d76f65e29d24848e1a4060a735b3bbe2053d
Björn Bartels <bartels@dragon-projects.net>
2016-02-19 23:16:21 +0100

    update user data forms

--- src/Admin/Model/User.php
+++ src/Admin/Model/User.php
@@ -45,25 +45,48 @@ class User implements InputFilterAwareInterface
 	protected $serviceManager;
 	protected $serviceLocator;
 	
-	public function exchangeArray($data)
+	/**
+	 * set user's object property data
+	 * @param array $data
+	 * @param boolean $forceEncryptPassword
+	 * @return \Admin\Entity\User
+	 */
+	public function exchangeArray ($data = array(), $forceEncryptPassword = false)
 	{
-		$this->id		    = (isset($data['id'])) ? $data['id'] : null;
-		$this->user_id		= (isset($data['user_id'])) ? $data['user_id'] : null;
-		$this->username		= (isset($data['username'])) ? $data['username'] : null;
-		$this->email		= (isset($data['email'])) ? $data['email'] : null;
-		$this->street		= (isset($data['street'])) ? $data['street'] : '';
-		$this->city			= (isset($data['city'])) ? $data['city'] : '';
-		$this->phone		= (isset($data['phone'])) ? $data['phone'] : '';
-		/* $bcrypt = new Bcrypt;
-		$bcrypt->setCost(null); // @TODO $this->getUserService()->getOptions()->getPasswordCost());
-		$this->password		= (isset($data['password'])) ? $bcrypt->create($data['password']) : null; */
-		$this->password		= (isset($data['password'])) ? $data['password'] : null;
-		$this->display_name	= (isset($data['display_name'])) ? $data['display_name'] : '';
-		$this->state		= (isset($data['state'])) ? $data['state'] : 0;
-		$this->aclrole		= (isset($data['aclrole'])) ? $data['aclrole'] : 'public';
-		$this->token		= (isset($data['token'])) ? $data['token'] : '';
-	}
+		if ( isset($data['id']) && !empty($data["id"]) ) {
+			$this->id		    = ($data['id']);
+			$this->user_id		= ($data['id']);
+		} elseif ( isset($data['user_id']) && !empty($data["user_id"]) ) {
+			$this->id		    = ($data['user_id']);
+			$this->user_id		= ($data['user_id']);
+		}
+		$this->username		= (isset($data['username'])) ? $data['username'] : $this->username;
+		$this->email		= (isset($data['email'])) ? $data['email'] : $this->email;
+		if ( isset($data['displayName']) ) {
+			$this->display_name	= $data['displayName'];
+			$this->displayName	= $data['displayName'];
+		} elseif ( isset($data['display_name']) ) {
+			$this->display_name	= $data['display_name'];
+			$this->displayName	= $data['display_name'];
+		}
+		if ( isset($data["password"]) && $forceEncryptPassword ) {
+			$bcrypt = new Bcrypt;
+			$bcrypt->setCost(null); // @TODO $this->getUserService()->getOptions()->getPasswordCost());
+			$data["password"] = $bcrypt->create($data['password']);
+		}
+		$this->password		= (isset($data['password'])) ? $data['password'] : $this->password;
+		$this->state		= (isset($data['state'])) ? $data['state'] : $this->state;
+		$this->aclrole		= (isset($data['aclrole'])) ? $data['aclrole'] : $this->aclrole;
+		$this->token		= (isset($data['token'])) ? $data['token'] : $this->token;
 
+		$this->street		= (isset($data['street'])) ? $data['street'] : $this->street;
+		$this->city			= (isset($data['city'])) ? $data['city'] : $this->city;
+		$this->phone		= (isset($data['phone'])) ? $data['phone'] : $this->phone;
+		
+		return $this;
+	}
+	
+	
 	public function getArrayCopy()
 	{
 		return get_object_vars($this);
