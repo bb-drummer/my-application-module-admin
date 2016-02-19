125b17973a18d4a218ac393f75c6cec3cdf9a440
Björn Bartels <bartels@dragon-projects.net>
2016-02-19 13:18:07 +0100

    update user model/entity

--- src/Admin/Entity/User.php
+++ src/Admin/Entity/User.php
@@ -197,11 +197,29 @@ class User extends ZfcUser
 	
 	
 	
+	public function exchangeArray($data)
+	{
+		$this->id		    = (isset($data['id'])) ? $data['id'] : null;
+		$this->user_id		= (isset($data['user_id'])) ? $data['user_id'] : null;
+		$this->username		= (isset($data['username'])) ? $data['username'] : null;
+		$this->email		= (isset($data['email'])) ? $data['email'] : null;
+		$this->street		= (isset($data['street'])) ? $data['street'] : '';
+		$this->city			= (isset($data['city'])) ? $data['city'] : '';
+		$this->phone		= (isset($data['phone'])) ? $data['phone'] : '';
+		/* $bcrypt = new Bcrypt;
+		$bcrypt->setCost(null); // @TODO $this->getUserService()->getOptions()->getPasswordCost());
+		$this->password		= (isset($data['password'])) ? $bcrypt->create($data['password']) : null; */
+		$this->password		= (isset($data['password'])) ? $data['password'] : null;
+		$this->display_name	= (isset($data['display_name'])) ? $data['display_name'] : '';
+		$this->state		= (isset($data['state'])) ? $data['state'] : 0;
+		$this->aclrole		= (isset($data['aclrole'])) ? $data['aclrole'] : 'public';
+		$this->token		= (isset($data['token'])) ? $data['token'] : '';
+	}
+
 	/*public function getArrayCopy()
 	{
-		return $this->__getArrayCopy();
+		return get_object_vars($this);
 	}*/
-	
 	
 	
 	
