32f567b350b648aa0603437617dcf73c28fc247d
Björn Bartels <bartels@dragon-projects.net>
2016-02-19 11:30:13 +0100

    update user forms

--- src/Admin/Form/UserDataForm.php
+++ src/Admin/Form/UserDataForm.php
@@ -22,7 +22,7 @@ class UserDataForm extends Form
 	public function __construct($name = null)
 	{
 		// we want to ignore the name passed
-		parent::__construct('user');
+		parent::__construct('userdata');
 		$this->setAttribute('method', 'post');
 		$this->add(array(
 			'name' => 'user_id',
