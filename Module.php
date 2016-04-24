2ab089a1a7378fa6495052a12f459502d3271dc3
Björn Bartels <bartels@dragon-projects.net>
2016-04-24 22:17:32 +0200

    update redirect callback and zfcuser controller config

--- Module.php
+++ Module.php
@@ -435,16 +435,16 @@ class Module implements AutoloaderProviderInterface, ServiceLocatorAwareInterfac
     {
         return array(
             'factories' => array(
-                /* 'zfcuser' => function ($controllerManager) {
-                        /** @var \Zend\Mvc\Controller\ControllerManager $controllerManager * /
+                'zfcuser' => function ($controllerManager) {
+                        /** @var \Zend\Mvc\Controller\ControllerManager $controllerManager */
                         $serviceManager = $controllerManager->getServiceLocator();
-                        /** @var \Admin\Controller\RedirectCallback $redirectCallback * /
+                        /** @var \Admin\Controller\RedirectCallback $redirectCallback */
                         $redirectCallback = $serviceManager->get('zfcuser_redirect_callback');
-                        /** @var \Admin\Controller\ZfcuserController $controller * /
+                        /** @var \Admin\Controller\ZfcuserController $controller */
                         $controller = new ZfcuserController($redirectCallback);
 
                         return $controller;
-                },*/
+                },
             ),
         );
     }
@@ -455,16 +455,16 @@ class Module implements AutoloaderProviderInterface, ServiceLocatorAwareInterfac
         'factories' => array(
                     
 
-               /* 'zfcuser_redirect_callback' => function ($sm) {
-                    /** @var RouteInterface $router  * /
+               'zfcuser_redirect_callback' => function ($sm) {
+                    /** @var RouteInterface $router  */
                     $router = $sm->get('router');
-                    /** @var Application $application  * /
+                    /** @var Application $application  */
                     $application = $sm->get('Application');
-                    /** @var ModuleOptions $options  * /
+                    /** @var ModuleOptions $options  */
                     $options = $sm->get('zfcuser_module_options');
 
                     return new RedirectCallback($application, $router, $options);
-                },*/
+                },
         		
             ),
         );
