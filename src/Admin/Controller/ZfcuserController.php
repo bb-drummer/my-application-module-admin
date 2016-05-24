08eba7cbfa970a28485bf37425baf582ad835053
Björn Bartels <bartels@dragon-projects.net>
2016-05-24 14:35:52 +0200

    update zfc-user controller

--- src/Admin/Controller/ZfcuserController.php
+++ src/Admin/Controller/ZfcuserController.php
@@ -175,8 +175,28 @@ class ZfcuserController extends UserController
     public function onDispatch(MvcEvent $e)
     {
         $oEvent = $this->applyToolbarOnDispatch($e);
-        $result = parent::onDispatch($oEvent);
-        return $result;
+        
+        $routeMatch = $e->getRouteMatch();
+        if (!$routeMatch) {
+            /**
+             * @todo Determine requirements for when route match is missing.
+             *       Potentially allow pulling directly from request metadata?
+             */
+            throw new Exception\DomainException('Missing route matches; unsure how to retrieve action');
+        }
+
+        $action = $routeMatch->getParam('action', 'not-found');
+        $method = static::getMethodFromAction($action);
+
+        if (!method_exists($this, $method)) {
+            $method = 'notFoundAction';
+        }
+
+        $actionResponse = $this->$method();
+
+        $e->setResult($actionResponse);
+
+        return $actionResponse;
     }
     
     /**
